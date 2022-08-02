f045386b9f41ba00a2bbcabf0f34b765d6d98f41
pmpc94 <pmig.pc@gmail.com>
2022-08-02 18:12:30 +0100

    fix pipeline

--- server/api/raffle.js
+++ server/api/raffle.js
@@ -3,7 +3,8 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-app.get('/raffle', async (req, res) => {
+// eslint-disable-next-line require-await
+app.get('/raffle', async (_req, res) => {
     const raffle = [1,2,3,4,5,6]
 
     res.json({ raffle})
