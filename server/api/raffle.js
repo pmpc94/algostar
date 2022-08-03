ced1743b3d7ea53abbe738f22482eb53b79b6fc4
pmpc94 <pmig.pc@gmail.com>
2022-08-03 22:21:19 +0100

    add prettier rules

--- server/api/raffle.js
+++ server/api/raffle.js
@@ -5,9 +5,9 @@ app.use(express.json())
 
 // eslint-disable-next-line require-await
 app.get('/raffle', async (_req, res) => {
-  const raffle = [1, 2, 3, 4, 5, 6]
+	const raffle = [1, 2, 3, 4, 5, 6]
 
-  res.json({ raffle })
+	res.json({ raffle })
 })
 
 module.exports = app
--- server/utils/index.ts
+++ server/utils/index.ts
@@ -1,13 +1,13 @@
 const generateRandomList = function (limit: Number): Array<Number> {
-  let i = 0
-  const list = []
-  let randomNumber = 0
-  while (i < limit) {
-    i++
-    randomNumber = Math.floor(Math.random() * Number(limit))
-    list.push(randomNumber)
-  }
-  return list
+	let i = 0
+	const list = []
+	let randomNumber = 0
+	while (i < limit) {
+		i++
+		randomNumber = Math.floor(Math.random() * Number(limit))
+		list.push(randomNumber)
+	}
+	return list
 }
 
 module.exports = { generateRandomList }
