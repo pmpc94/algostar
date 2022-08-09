dce6410e7211ae2f46942475c4e7e40a5911b982
pmpc94 <pmig.pc@gmail.com>
2022-08-09 23:49:28 +0100

    prepare before prisma generate

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,12 @@
-import { createServer } from '@graphql-yoga/node'
 import express from 'express'
 import { resolvers } from './resolvers/index'
 import prisma from './prisma'
 
-const app = express()
+const { ApolloServer } = require('apollo-server-express')
 
-const server = createServer({
-  schema: {
-    typeDefs: './models/schema.graphql',
-    resolvers
-  },
+const server = ApolloServer({
+  typeDefs: './models/schema.graphql',
+  resolvers,
   context(request: object) {
     return {
       prisma,
@@ -18,8 +15,13 @@ const server = createServer({
   }
 })
 
-app.use('/graphql', server)
+const app = express()
 
-app.listen(3000, () => {
-  console.log('GraphQL API located at http://localhost:4000/graphql')
+server.start().then(() => {
+  server.applyMiddleware({ app })
 })
+
+export default {
+  path: '/api',
+  handler: app
+}
--- server/prisma.ts
+++ server/prisma.ts
@@ -1,7 +1,7 @@
 import { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
-  typeDefs: 'server/generated/prisma.graphql',
+  typeDefs: 'generated/prisma.graphql',
   endpoint: 'http://localhost:4466'
 })
 
